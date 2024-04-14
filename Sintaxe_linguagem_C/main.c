7f16506398d3d89e4737f6cc6503a2f0ee77de01
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-04-14 20:58:35 -0300

    feat: variables and data types in C

---
+++ Sintaxe_linguagem_C/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int x = 999;     // variavel inteira %d
+float c = 80.9;  // variave real %f
+double t = 60.8; // variavel real(dobro de precisão, usadas em grandes
+                 // processamentos, gasta mais memória) %c
+char a = 'F';     // variavel char recebe um unico caractere %c
+
+int main() {
+  printf("O valor da variavel inteira é: %d\n", x);
+  printf("O valor da variavel real é: %.2f\n", c);
+  printf("O valor da variavel real(double) é: %.2f\n", t);
+  printf("O valor da variavel caractere é: %c", a);
+  return 0;
+}
